a01121c3840946157ca1b969db06c1894f7dd508
Ashan-Home <ashanb58@gmail.com>
2020-02-16 22:20:25 +0530

    modules compile and package

--- java9-modular-example/helloworld/src/com/javamodularity/helloworld/HelloWorld.java
+++ java9-modular-example/helloworld/src/com/javamodularity/helloworld/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
    public static void main(String... args) {
       System.out.println("Hello Modular World!");
    }
+   // compile modules javac -d output/helloworld helloworld/src/com/javamodularity/helloworld/HelloWorld.java helloworld/src/module-info.java
 
+   // jar -cfe mods/helloworld.jar com.javamodularity.helloworld.HelloWorld -C output/helloworld .
 }
