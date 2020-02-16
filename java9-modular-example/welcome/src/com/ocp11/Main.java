1a92b41331b2dc4060e0dcb3761358b0853e5a60
Ashan-Home <ashanb58@gmail.com>
2020-02-16 19:42:31 +0530

    testing

--- java9-modular-example/welcome/src/com/ocp11/Main.java
+++ java9-modular-example/welcome/src/com/ocp11/Main.java
@@ -2,6 +2,8 @@ package com.ocp11;
 
 import com.ocp11.world.HelloWorld;
 import com.ocp11.youtube.HelloYouTube;
+//import main.java1.DEF; can not access
+import main.java2.ABC; // class without a module.
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +12,7 @@ public class Main {
 
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.print();
+        ABC abc = new ABC();
+        //DEF def = new DEF();
     }
 }
---
+++ java9-modular-example/welcome/src/main/java2/ABC.java
@@ -0,0 +1,4 @@
+package main.java2;
+
+public class ABC {
+}
