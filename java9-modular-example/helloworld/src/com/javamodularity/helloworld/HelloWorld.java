f70c6cff427a1d151b766ef9873f11f22029feaa
Ashan-Home <ashanb58@gmail.com>
2020-02-22 20:19:34 +0530

    Java9 modularity-Chapter 2 and 3

--- java9-modular-example/helloworld/src/com/javamodularity/helloworld/HelloWorld.java
+++ java9-modular-example/helloworld/src/com/javamodularity/helloworld/HelloWorld.java
@@ -5,7 +5,22 @@ public class HelloWorld {
    public static void main(String... args) {
       System.out.println("Hello Modular World!");
    }
-   // compile modules javac -d output/helloworld helloworld/src/com/javamodularity/helloworld/HelloWorld.java helloworld/src/module-info.java
+   // compile modules:  javac -d output/helloworld helloworld/src/com/javamodularity/helloworld/HelloWorld.java helloworld/src/module-info.java
 
-   // jar -cfe mods/helloworld.jar com.javamodularity.helloworld.HelloWorld -C output/helloworld .
+   // package: jar -cfe mods/helloworld.jar com.javamodularity.helloworld.HelloWorld -C output/helloworld .
+
+   // run from Class: java --module-path output --module helloworld/com.javamodularity.helloworld.HelloWorld
+
+   // run from Class (short form): java -p output -m helloworld/com.javamodularity.helloworld.HelloWorld
+
+   // run from packaged module jar (short form): java -p mods  -m helloworld
+
+   // run (see the resolution) java --show-module-resolution --limit-modules java.base -p mods -m helloworld
+
+   // to see the dependent modules: jdeps --module-path output --module helloworld
+
+  // path  tree : tree /a /f
+
+   // create a image: jlink --module-path mods --add-modules helloworld --launcher hello=helloworld --output helloworld-image
+
 }
